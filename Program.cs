2ad2f54ad0d0ba478b72599d9049735e21f2a436
Kara Hall <karalynn.inc@gmail.com>
2023-05-22 15:44:36 -0400

    phase 7

--- Program.cs
+++ Program.cs
@@ -10,14 +10,59 @@ MooseSays("H I, I'M  E N T H U S I A S T I C !");
 MooseSays("I really am enthusiastic");
 
 // Ask a question
-bool isTrue = MooseAsks("Is Canada real?");
-if (isTrue)
+CanadaQuestion();
+
+
+void CanadaQuestion()
 {
-    MooseSays("Really? It seems very unlikely.");
+    bool isTrue = MooseAsks("Is Canada real?");
+    if (isTrue)
+    {
+        MooseSays("Really? It seems very unlikely.");
+    }
+    else
+    {
+        MooseSays("I  K N E W  I T !!!");
+    }
 }
-else
+
+void EnthusiasticQuestion()
 {
-    MooseSays("I  K N E W  I T !!!");
+    bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
+    if (isEnthusiastic)
+    {
+        MooseSays("Yay!");
+    }
+    else
+    {
+        MooseSays("You should try it!");
+    }
+}
+
+void LoveCSharpQuestion()
+{
+    bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
+    if (doesLoveCSharp)
+    {
+        MooseSays("Good job sucking up to your instructor!");
+    }
+    else
+    {
+        MooseSays("You will...oh, yes, you will...");
+    }
+}
+
+void SecretQuestion()
+{
+    bool wantsSecret = MooseAsks("Do you want to know a secret?");
+    if (wantsSecret)
+    {
+        MooseSays("ME TOO!!!! I love secrets...tell me one!");
+    }
+    else
+    {
+        MooseSays("Oh, no...secrets are the best, I love to share them!");
+    }
 }
 
 void MooseSays(string message)
@@ -72,4 +117,9 @@ bool MooseAsks(string question)
         return false;
     };
 } 
+// Ask some questions
+CanadaQuestion();
+EnthusiasticQuestion();
+LoveCSharpQuestion();
+SecretQuestion();
 
