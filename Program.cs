3cb81dd3860e832bcaeaf9160a09dcf6418b6a0e
Kara Hall <karalynn.inc@gmail.com>
2023-05-22 14:17:02 -0400

    phase 4

--- Program.cs
+++ Program.cs
@@ -5,10 +5,13 @@ Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
 Console.WriteLine("--------------------------------------------");
 Console.WriteLine();
 
+// Let the moose speak!
+MooseSays("H I, I'M  E N T H U S I A S T I C !");
+MooseSays("I really am enthusiastic");
 
-void MooseSays()
+void MooseSays(string message)
 {
-    Console.WriteLine(@"
+    Console.WriteLine($@"
                                        _.--^^^--,
                                     .'          `\
   .-^^^^^^-.                      .'              |
@@ -25,7 +28,7 @@ void MooseSays()
                '-'    | /       \ |
                       |  .-. .-.  |
                       \ / o| |o \ /
-                       |   / \   |    H I, I'M  E N T H U S I A S T I C !
+                       |   / \   |     {message}
                       / `^`   `^` \
                      /             \
                     | '._.'         \
@@ -38,4 +41,3 @@ void MooseSays()
     ");
 }
 
-MooseSays();
